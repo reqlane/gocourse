b4151ec9b5c4f179c9776af7c20d9c0771c547ba
reqlane <107763485+reqlane@users.noreply.github.com>
2026-03-01 18:47:59 +0200

    Custom errors

---
+++ intermediate/custom_error.go
@@ -0,0 +1,50 @@
+package intermediate
+
+import (
+	"errors"
+	"fmt"
+)
+
+func main() {
+
+	err := doSomething()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("Operation completed successfully!")
+}
+
+type customError struct {
+	code    int
+	message string
+	er      error
+}
+
+// Error returns the error message. Implementing Error() method of error interface
+func (e *customError) Error() string {
+	return fmt.Sprintf("Error %d: %s, %v\n", e.code, e.message, e.er)
+}
+
+// Functions that returns a custom error
+// func doSomething() error {
+// 	return &customError{
+// 		code:    500,
+// 		message: "Something went wrong!",
+// 	}
+// }
+
+func doSomething() error {
+	if err := doSomethingElse(); err != nil {
+		return &customError{
+			code:    500,
+			message: "Something went wrong",
+			er:      err,
+		}
+	}
+	return nil
+}
+
+func doSomethingElse() error {
+	return errors.New("internal error")
+}
