9aed49d3c1e40555adf92d1d6562e68aad4904fb
reqlane <107763485+reqlane@users.noreply.github.com>
2026-03-03 19:36:10 +0200

    Base64 encoding

---
+++ intermediate/base64encoding.go
@@ -0,0 +1,27 @@
+package intermediate
+
+import (
+	"encoding/base64"
+	"fmt"
+)
+
+func main() {
+
+	data := []byte("He~lo, Base64 Encoding")
+
+	// Encode to Base64
+	encoded := base64.StdEncoding.EncodeToString(data)
+	fmt.Println(encoded)
+
+	// Decode from Base64
+	decoded, err := base64.StdEncoding.DecodeString(encoded)
+	if err != nil {
+		fmt.Println("Error decoding string:", err)
+		return
+	}
+	fmt.Println("Decoded string:", string(decoded))
+
+	// URL safe (avoid '/' and '+')
+	urlSafeEncoded := base64.URLEncoding.EncodeToString(data)
+	fmt.Println("URL Safe encoded:", urlSafeEncoded)
+}
