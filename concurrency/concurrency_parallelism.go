7242db397459cf8730fcb86787a1b1a27ff493ca
reqlane <107763485+reqlane@users.noreply.github.com>
2026-03-10 13:14:07 +0200

    Concurrency vs parallelism

---
+++ concurrency/concurrency_parallelism.go
@@ -0,0 +1,53 @@
+package concurrency
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+	"time"
+)
+
+// func printNumbers() {
+// 	for i := range 5 {
+// 		fmt.Println(time.Now())
+// 		fmt.Println(i)
+// 		time.Sleep(500 * time.Millisecond)
+// 	}
+// }
+
+// func printLetters() {
+// 	for _, letter := range "ABCDE" {
+// 		fmt.Println(time.Now())
+// 		fmt.Println(string(letter))
+// 		time.Sleep(500 * time.Millisecond)
+// 	}
+// }
+
+func heavyTask(id int, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	fmt.Printf("Task %d is starting\n", id)
+	for range 100_000_000 {
+
+	}
+	fmt.Println(time.Now())
+	fmt.Printf("Task %d is finished\n", id)
+}
+
+func main() {
+
+	numThreads := 12
+	runtime.GOMAXPROCS(numThreads)
+	var wg sync.WaitGroup
+
+	for i := range numThreads {
+		wg.Add(1)
+		go heavyTask(i, &wg)
+	}
+
+	wg.Wait()
+
+	// go printNumbers()
+	// go printLetters()
+	// time.Sleep(3 * time.Second)
+}
