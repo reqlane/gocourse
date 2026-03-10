ccf3dd7958f64b32d23c3c540dfca59bcd04d294
reqlane <107763485+reqlane@users.noreply.github.com>
2026-03-10 13:43:57 +0200

    Deadlocks

---
+++ concurrency/deadlocks.go
@@ -0,0 +1,38 @@
+package concurrency
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+
+	var mu1, mu2 sync.Mutex
+
+	// Goroutine 1: lock mu1 -> lock mu2
+	// Goroutine 2: lock mu2 -> lock mu1
+	go func() {
+		mu1.Lock()
+		fmt.Println("Goroutine 1 locked mu1")
+		time.Sleep(time.Second)
+		mu2.Lock()
+		fmt.Println("Goroutine 1 locked mu2")
+		mu1.Unlock()
+		mu2.Unlock()
+	}()
+
+	go func() {
+		mu2.Lock()
+		fmt.Println("Goroutine 2 locked mu2")
+		time.Sleep(time.Second)
+		mu1.Lock()
+		fmt.Println("Goroutine 2 locked mu1")
+		mu2.Unlock()
+		mu1.Unlock()
+	}()
+
+	// time.Sleep(3 * time.Second)
+	// fmt.Println("Main function completed")
+	select {}
+}
